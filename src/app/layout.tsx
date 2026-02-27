96ca4eb7aba62c4af402790929ebcb5a4241264e
Dhanush Kumar <dhanushkumar.merndev@gmail.com>
2026-02-27 16:10:47 +0530

    Final 8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,13 @@ export const metadata: Metadata = {
     url: "https://tansimotors.netlify.app",
     siteName: "Tansi Honda",
     images: [
-  {
-    url: "/api/og",
-    width: 1200,
-    height: 630,
-    alt: "Tansi Honda Hiring",
-  },
-],
+      {
+        url: "/og-image.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "Tansi Honda Hiring",
+      },
+    ],
     locale: "en_US",
     type: "website",
   },
